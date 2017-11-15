5af0545d7ff3ec8cf9ff4697dc487a648f7aafbd
Raca <aracki.ivan@gmail.com>
2017-11-15 15:41:03 +0100

    Add mongodb auth and getNumVisitors()

--- mongodb/visitor.go
+++ mongodb/visitor.go
@@ -10,9 +10,30 @@ import (
 )
 
 const (
+	host       = "localhost"
+	username   = "raca"
+	pwd        = "scofield"
 	db         = "aracki"
 	c_visitors = "visitors"
 )
+
+func getSession() *mgo.Session {
+
+	info := &mgo.DialInfo{
+		Addrs:    []string{host},
+		Timeout:  60 * time.Second,
+		Database: db,
+		Username: username,
+		Password: pwd,
+	}
+
+	session, err := mgo.DialWithInfo(info)
+	if err != nil {
+		panic(err)
+	}
+
+	return session
+}
 
 func InsertVisitor(r *http.Request) {
 
@@ -21,16 +42,12 @@ func InsertVisitor(r *http.Request) {
 	//logg("Inserting visitor with ip=" + ip)
 	fmt.Println("Inserting visitor with ip=" + ip)
 
-	session, err := mgo.Dial("localhost")
-	if err != nil {
-		panic(err)
-
-	}
+	session := getSession()
 	defer session.Close()
 
 	c := session.DB(db).C(c_visitors)
 
-	err = c.Insert(bson.M{
+	err := c.Insert(bson.M{
 		"ip":   ip,
 		"date": time.Now(),
 	})
@@ -38,4 +55,18 @@ func InsertVisitor(r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func GetNumberOfVisitors() int {
+
+	session := getSession()
+	defer session.Close()
+
+	c := session.DB(db).C(c_visitors)
+	totalNum, err := c.Count()
+	if err != nil {
+		panic(err)
+	}
+
+	return totalNum
 }
