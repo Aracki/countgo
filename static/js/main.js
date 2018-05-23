1345c9e63cf9011ac947fcafaddd089efe0814e1
Ivan Aracki <aracki.ivan@gmail.com>
2018-05-23 16:49:46 +0200

    Add static css/js/html files to project

---
+++ static/js/main.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // ping golang unique visitor counter
+    getRequest("/count", function (request) {
+        var response = request.currentTarget.response || request.target.responseText;
+        var counter = document.getElementById("counter_text");
+
+        if (counter != null) {
+            document.getElementById("counter_text")
+                .innerHTML = "[" + response + " unique visitors]";
+        }
+    })
+});
+
+function getRequest(url, success) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = success;
+    xhr.send();
+    return xhr;
+}
+
+$('#back_to_home1').on('click', function () {
+    window.location.href = 'index.html';
+});
+
+$('#back_to_home2').on('click', function () {
+    window.location.href = 'index.html';
+});
+
+$('#yt_text').on('click', function () {
+    window.location.href = "yt.html";
+});
